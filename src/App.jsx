567ab158e627a687740e476b2d3d071cecb5c57c
morrisjustin21 <morris.justin21@gmail.com>
2026-08-27 09:59:15 -0500

    Implement routing with BrowserRouter and Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,14 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Leaderboard from './pages/Leaderboard.jsx'
+import Admin from './pages/Admin.jsx'
 
 export default function App() {
-  return <Leaderboard />
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Leaderboard />} />
+        <Route path="/admin" element={<Admin />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
