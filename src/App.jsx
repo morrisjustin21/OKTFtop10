8c360ddd859e2b6213a8f9c13cafcd8243cf98ab
morrisjustin21 <morris.justin21@gmail.com>
2026-08-28 10:33:33 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Leaderboard from './pages/Leaderboard.jsx'
 import Admin from './pages/Admin.jsx'
 import AthleteProfile from './pages/AthleteProfile.jsx'
+import PrintReport from './pages/PrintReport.jsx'
 
 export default function App() {
   return (
@@ -10,6 +11,7 @@ export default function App() {
         <Route path="/" element={<Leaderboard />} />
         <Route path="/admin" element={<Admin />} />
         <Route path="/athlete/:athleteId" element={<AthleteProfile />} />
+        <Route path="/print" element={<PrintReport />} />
       </Routes>
     </BrowserRouter>
   )
