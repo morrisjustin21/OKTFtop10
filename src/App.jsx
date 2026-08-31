bcd21dd9fb056d0222a06664aa26b1254208ddcb
morrisjustin21 <morris.justin21@gmail.com>
2026-08-31 12:27:47 -0500

    Add MeetsList route to App component

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Leaderboard from './pages/Leaderboard.jsx'
 import Admin from './pages/Admin.jsx'
 import AthleteProfile from './pages/AthleteProfile.jsx'
 import PrintReport from './pages/PrintReport.jsx'
+import MeetsList from './pages/MeetsList.jsx'
 
 export default function App() {
   return (
@@ -12,6 +13,7 @@ export default function App() {
         <Route path="/admin" element={<Admin />} />
         <Route path="/athlete/:athleteId" element={<AthleteProfile />} />
         <Route path="/print" element={<PrintReport />} />
+        <Route path="/meets" element={<MeetsList />} />
       </Routes>
     </BrowserRouter>
   )
